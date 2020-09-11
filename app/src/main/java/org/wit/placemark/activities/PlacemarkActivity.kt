243fbad146032ca7d9ef09059b24f39dfc155dcf
Eamonn de Leastar <edeleastar@wit.ie>
2020-09-11 11:01:49 +0100

    new PlacemarkList activiity

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -7,30 +7,36 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
   var placemark = PlacemarkModel()
-  val placemarks = ArrayList<PlacemarkModel>()
+  lateinit var app: MainApp
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
+    info("Placemark Activity started..")
+
+    app = application as MainApp
 
     btnAdd.setOnClickListener() {
       placemark.title = placemarkTitle.text.toString()
       placemark.description = description.text.toString()
       if (placemark.title.isNotEmpty()) {
-        placemarks.add(placemark.copy())
+        app.placemarks.add(placemark.copy())
         info("add Button Pressed: ${placemark}")
-        for (i in placemarks.indices) {
-          info("Placemark[$i]:${this.placemarks[i]}")
+        for (i in app.placemarks.indices) {
+          info("Placemark[$i]:${app.placemarks[i]}")
         }
-      }
-      else {
-        toast ("Please Enter a title")
+        setResult(AppCompatActivity.RESULT_OK)
+        finish()
+      } else {
+        toast("Please Enter a title")
       }
     }
   }
 }
+
