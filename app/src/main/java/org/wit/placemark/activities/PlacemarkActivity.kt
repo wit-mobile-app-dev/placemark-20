0dadefa037b7ce508e4eee24598bd629ca40fac7
Eamonn de Leastar <edeleastar@wit.ie>
2020-10-12 09:26:48 +0100

    introduce location model + make marker dragable

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -15,6 +15,7 @@ import org.wit.placemark.helpers.readImage
 import org.wit.placemark.helpers.readImageFromPath
 import org.wit.placemark.helpers.showImagePicker
 import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.Location
 import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
@@ -68,7 +69,8 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     }
 
     placemarkLocation.setOnClickListener {
-      startActivity (intentFor<MapActivity>())
+      val location = Location(52.245696, -7.139102, 15f)
+      startActivity (intentFor<MapActivity>().putExtra("location", location))
     }
   }
 
