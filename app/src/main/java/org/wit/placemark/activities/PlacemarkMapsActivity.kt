00b47de4673459d0d38f9f7eb589c0e9e79a5be4
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-09 12:24:59 +0000

    lifecycle events for map to render correctly

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import org.wit.placemark.R
-
 import kotlinx.android.synthetic.main.activity_placemark_maps.*
-
 
 class PlacemarkMapsActivity : AppCompatActivity() {
 
@@ -15,8 +13,32 @@ class PlacemarkMapsActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark_maps)
-    toolbar.title = title
     setSupportActionBar(toolbar)
     mapView.onCreate(savedInstanceState);
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mapView.onDestroy()
+  }
+
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView.onLowMemory()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    mapView.onPause()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mapView.onResume()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    mapView.onSaveInstanceState(outState)
   }
 }
