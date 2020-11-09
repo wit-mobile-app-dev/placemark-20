53322e6aff6611d0f22634fcc59ad745ef9b47da
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-09 12:22:30 +0000

    include CardView + MapView directly onto activity

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -1,15 +1,22 @@
 package org.wit.placemark.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_placemark_list.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
 import org.wit.placemark.R
 
+import kotlinx.android.synthetic.main.activity_placemark_maps.*
+
+
 class PlacemarkMapsActivity : AppCompatActivity() {
+
+  lateinit var map: GoogleMap
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark_maps)
-    setSupportActionBar(toolbar)
     toolbar.title = title
+    setSupportActionBar(toolbar)
+    mapView.onCreate(savedInstanceState);
   }
 }
