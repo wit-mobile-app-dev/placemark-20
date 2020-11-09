23e2d50ebefdef46adf44db93a8cbfd38604d64a
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-09 12:35:33 +0000

    display markers for placemarks + zoom to last one rendered

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -2,12 +2,17 @@ package org.wit.placemark.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.placemark.R
 import kotlinx.android.synthetic.main.activity_placemark_maps.*
+import org.wit.placemark.main.MainApp
 
 class PlacemarkMapsActivity : AppCompatActivity() {
 
+  lateinit var app: MainApp
   lateinit var map: GoogleMap
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +20,23 @@ class PlacemarkMapsActivity : AppCompatActivity() {
     setContentView(R.layout.activity_placemark_maps)
     setSupportActionBar(toolbar)
     mapView.onCreate(savedInstanceState);
+    app = application as MainApp
+
+    mapView.getMapAsync {
+      map = it
+      configureMap()
+    }
   }
 
+  fun configureMap() {
+    map.uiSettings.setZoomControlsEnabled(true)
+    app.placemarks.findAll().forEach {
+      val loc = LatLng(it.lat, it.lng)
+      val options = MarkerOptions().title(it.title).position(loc)
+      map.addMarker(options).tag = it.id
+      map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+    }
+  }
   override fun onDestroy() {
     super.onDestroy()
     mapView.onDestroy()
