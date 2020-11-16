21f3bac5fb14bec5e529cf4ca1b6a14301255b11
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-16 13:11:06 +0000

    refactor Placemark and PlacemarkList presenter/views to use BasePresenter/BaseView

--- app/src/main/java/org/wit/placemark/views/placemark/PlacemarkView.kt
+++ app/src/main/java/org/wit/placemark/views/placemark/PlacemarkView.kt
@@ -1,7 +1,6 @@
 package org.wit.placemark.views.placemark
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -11,8 +10,9 @@ import org.jetbrains.anko.toast
 import org.wit.placemark.R
 import org.wit.placemark.helpers.readImageFromPath
 import org.wit.placemark.models.PlacemarkModel
+import org.wit.placemark.views.BaseView
 
-class PlacemarkView : AppCompatActivity(), AnkoLogger {
+class PlacemarkView : BaseView(), AnkoLogger {
 
   lateinit var presenter: PlacemarkPresenter
   var placemark = PlacemarkModel()
@@ -20,17 +20,17 @@ class PlacemarkView : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
-    toolbarAdd.title = title
-    setSupportActionBar(toolbarAdd)
 
-    presenter = PlacemarkPresenter(this)
+    init(toolbarAdd)
+
+    presenter = initPresenter (PlacemarkPresenter(this)) as PlacemarkPresenter
 
     chooseImage.setOnClickListener { presenter.doSelectImage() }
 
     placemarkLocation.setOnClickListener { presenter.doSetLocation() }
   }
 
-  fun showPlacemark(placemark: PlacemarkModel) {
+  override fun showPlacemark(placemark: PlacemarkModel) {
     placemarkTitle.setText(placemark.title)
     description.setText(placemark.description)
     placemarkImage.setImageBitmap(readImageFromPath(this, placemark.image))
@@ -41,24 +41,20 @@ class PlacemarkView : AppCompatActivity(), AnkoLogger {
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
     menuInflater.inflate(R.menu.menu_placemark, menu)
-    if (presenter.edit) menu.getItem(0).setVisible(true)
     return super.onCreateOptionsMenu(menu)
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     when (item?.itemId) {
+      R.id.item_delete -> {
+        presenter.doDelete()
+      }
       R.id.item_save -> {
         if (placemarkTitle.text.toString().isEmpty()) {
           toast(R.string.enter_placemark_title)
         } else {
           presenter.doAddOrSave(placemarkTitle.text.toString(), description.text.toString())
         }
-      }
-      R.id.item_delete -> {
-        presenter.doDelete()
-      }
-      R.id.item_cancel -> {
-        presenter.doCancel()
       }
     }
     return super.onOptionsItemSelected(item)
@@ -69,5 +65,9 @@ class PlacemarkView : AppCompatActivity(), AnkoLogger {
     if (data != null) {
       presenter.doActivityResult(requestCode, resultCode, data)
     }
+  }
+
+  override fun onBackPressed() {
+    presenter.doCancel()
   }
 }
