1371c240f1d99bcd4e968507170db38d58d69580
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-16 11:40:15 +0000

    PlacemarkLocationView/Presenter replaces MapActivity

---
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkLocationView.kt
@@ -0,0 +1,44 @@
+package org.wit.placemark.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Marker
+import org.wit.placemark.R
+
+class PlacemarkLocationView : AppCompatActivity(), GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
+
+  lateinit var map: GoogleMap
+  lateinit var presenter: PlacemarkLocationPresenter
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_map)
+    val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+    presenter = PlacemarkLocationPresenter(this)
+    mapFragment.getMapAsync {
+      map = it
+      map.setOnMarkerDragListener(this)
+      map.setOnMarkerClickListener(this)
+      presenter.initMap(map)
+    }
+  }
+
+  override fun onMarkerDragStart(marker: Marker) {}
+
+  override fun onMarkerDrag(marker: Marker) {}
+
+  override fun onMarkerDragEnd(marker: Marker) {
+    presenter.doUpdateLocation(marker.position.latitude, marker.position.longitude, map.cameraPosition.zoom)
+  }
+
+  override fun onBackPressed() {
+    presenter.doOnBackPressed()
+  }
+
+  override fun onMarkerClick(marker: Marker): Boolean {
+    presenter.doUpdateMarker(marker)
+    return false
+  }
+}
