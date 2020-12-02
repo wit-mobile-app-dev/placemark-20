ab93628be455eab493a9593ba9142b556085f7ba
Eamonn de Leastar <edeleastar@wit.ie>
2020-12-02 18:26:48 +0000

    new room based model

--- app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -1,17 +1,19 @@
 package org.wit.placemark.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class PlacemarkModel(var id: Long = 0,
+@Entity
+data class PlacemarkModel(@PrimaryKey(autoGenerate = true) var id: Long = 0,
                           var title: String = "",
                           var description: String = "",
                           var image: String = "",
                           var lat : Double = 0.0,
                           var lng: Double = 0.0,
                           var zoom: Float = 0f) : Parcelable
-
 @Parcelize
 data class Location(var lat: Double = 0.0,
                     var lng: Double = 0.0,
