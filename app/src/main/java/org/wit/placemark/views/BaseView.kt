c9b88ed7b4e32f29a0f98fc2643747b5df1f1581
Eamonn de Leastar <edeleastar@wit.ie>
2020-12-03 08:00:20 +0000

    render user email in toolbar

--- app/src/main/java/org/wit/placemark/views/BaseView.kt
+++ app/src/main/java/org/wit/placemark/views/BaseView.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.AnkoLogger
 import org.wit.placemark.models.Location
 
@@ -45,6 +46,10 @@ open abstract class BaseView() : AppCompatActivity(), AnkoLogger {
     toolbar.title = title
     setSupportActionBar(toolbar)
     supportActionBar?.setDisplayHomeAsUpEnabled(upEnabled)
+    val user = FirebaseAuth.getInstance().currentUser
+    if (user != null) {
+      toolbar.title = "${title}: ${user.email}"
+    }
   }
 
   fun initPresenter(presenter: BasePresenter): BasePresenter {
